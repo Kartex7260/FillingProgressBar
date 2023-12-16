1050541cf0a7570ac4332befd53ca20a2fb30383
Kirill Zhitko <kirillzhitko@gmail.com>
2023-12-16 12:22:52 +0300

    FEAT: атрибут android:enabled и отображение view в зависимости от isEnabled

--- fillingprogressbar/src/main/java/kanti/fillingprogressbar/FillingProgressBar.kt
+++ fillingprogressbar/src/main/java/kanti/fillingprogressbar/FillingProgressBar.kt
@@ -9,6 +9,7 @@ import android.view.View
 import androidx.annotation.ColorInt
 import androidx.annotation.Dimension
 import androidx.annotation.FloatRange
+import androidx.core.content.res.getBooleanOrThrow
 import androidx.core.content.res.getColorOrThrow
 import androidx.core.content.res.getDimensionOrThrow
 import androidx.core.content.res.getFloatOrThrow
@@ -17,7 +18,7 @@ class FillingProgressBar @JvmOverloads constructor(
 	context: Context,
 	attrs: AttributeSet? = null,
 	defStyleAttr: Int = 0,
-	defStyleRes: Int = R.style.FillingProgressBar
+	defStyleRes: Int = R.style.Widget_Kanti_FillingProgressBar
 ) : View(context, attrs, defStyleAttr, defStyleRes) {
 
 	@Dimension private var mDiameter: Float
@@ -25,9 +26,11 @@ class FillingProgressBar @JvmOverloads constructor(
 
 	@ColorInt private var mBackStrokeColor: Int
 	@ColorInt private var mFrontFillColor: Int
+	@ColorInt private var mDisabledColor: Int
 
 	@FloatRange(from = 0.0, to = 1.0) private var mProgress: Float
 	@FloatRange(from = 0.0, to = 1.0) private val mLightModifier: Float = 0.9f
+	@FloatRange(from = 0.0, to = 1.0) private val mOpacity: Float = 0.38f
 
 	private val paintBackStroke: Paint
 	private val paintFrontStroke: Paint
@@ -52,17 +55,41 @@ class FillingProgressBar @JvmOverloads constructor(
 		@ColorInt get() = mBackStrokeColor
 		set(@ColorInt value) {
 			mBackStrokeColor = value
-			paintBackStroke.color = value
-			invalidate()
+			if (isEnabled) {
+				paintBackStroke.color = value
+				invalidate()
+			}
 		}
 
 	var frontFillColor: Int
 		@ColorInt get() = mFrontFillColor
 		set(@ColorInt value) {
 			mFrontFillColor = value
-			paintFrontStroke.color = colorLightModifying()
-			paintFill.color = value
-			invalidate()
+			if (isEnabled) {
+				paintFrontStroke.color = colorLightModifying()
+				paintFrontStroke.computeAlpha(isEnabled)
+
+				paintFill.color = value
+				paintFill.computeAlpha(isEnabled)
+				invalidate()
+			}
+		}
+
+	var disabledColor: Int
+		@ColorInt get() = mDisabledColor
+		set(@ColorInt value) {
+			mDisabledColor = value
+			if (!isEnabled) {
+				paintBackStroke.color = mDisabledColor
+				paintBackStroke.computeBackStrokeAlpha(isEnabled)
+
+				paintFrontStroke.color = mDisabledColor
+				paintFrontStroke.computeAlpha(isEnabled)
+
+				paintFill.color = mDisabledColor
+				paintFill.computeAlpha(isEnabled)
+				invalidate()
+			}
 		}
 
 	var progress: Float
@@ -70,12 +97,13 @@ class FillingProgressBar @JvmOverloads constructor(
 		set(@FloatRange(from = 0.0, to = 1.0) value) {
 			checkProgress(value)
 			mProgress = value
-			paintFill.alpha = (255 * value).toInt()
-			paintFrontStroke.alpha = (255 * value).toInt()
+			paintFill.computeAlpha(isEnabled)
+			paintFrontStroke.computeAlpha(isEnabled)
 			invalidate()
 		}
 
 	init {
+		var enabled = true
 		context.theme.obtainStyledAttributes(
 			attrs,
 			R.styleable.FillingProgressBar,
@@ -88,10 +116,13 @@ class FillingProgressBar @JvmOverloads constructor(
 
 				mBackStrokeColor = getColorOrThrow(R.styleable.FillingProgressBar_backStrokeColor)
 				mFrontFillColor = getColorOrThrow(R.styleable.FillingProgressBar_frontFillColor)
+				mDisabledColor = getColorOrThrow(R.styleable.FillingProgressBar_disabledColor)
 
 				mProgress = getFloatOrThrow(R.styleable.FillingProgressBar_progress).also {
 					checkProgress(it)
 				}
+
+				enabled = getBooleanOrThrow(R.styleable.FillingProgressBar_android_enabled)
 			} finally {
 				recycle()
 			}
@@ -99,24 +130,42 @@ class FillingProgressBar @JvmOverloads constructor(
 
 		paintBackStroke = Paint(Paint.ANTI_ALIAS_FLAG).apply {
 			color = mBackStrokeColor
+			computeBackStrokeAlpha(enabled)
 			strokeWidth = mStrokeWidth
 			style = Paint.Style.STROKE
 		}
 		paintFrontStroke = Paint(Paint.ANTI_ALIAS_FLAG).apply {
 			color = colorLightModifying()
-			alpha = (255 * mProgress).toInt()
+			computeAlpha(enabled)
 			strokeWidth = mStrokeWidth
 			style = Paint.Style.STROKE
 		}
 		paintFill = Paint(Paint.ANTI_ALIAS_FLAG).apply {
 			color = mFrontFillColor
-			alpha = (255 * mProgress).toInt()
+			computeAlpha(enabled)
 		}
+
+		isEnabled = enabled
+	}
+
+	override fun setEnabled(enabled: Boolean) {
+		if (enabled) {
+			paintBackStroke.color = mBackStrokeColor
+			paintFrontStroke.color = mFrontFillColor
+			paintFill.color = mFrontFillColor
+		} else {
+			paintBackStroke.color = mDisabledColor
+			paintFrontStroke.color = mDisabledColor
+			paintFill.color = mDisabledColor
+
+		}
+		paintBackStroke.computeBackStrokeAlpha(enabled)
+		paintFrontStroke.computeAlpha(enabled)
+		paintFill.computeAlpha(enabled)
+		super.setEnabled(enabled)
 	}
 
 	override fun onDraw(canvas: Canvas) {
-		super.onDraw(canvas)
-
 		val centerX = paddingLeft + (mDiameter / 2f)
 		val centerY = paddingTop + (mDiameter / 2f)
 
@@ -155,5 +204,21 @@ class FillingProgressBar @JvmOverloads constructor(
 			(green * mLightModifier).toInt(),
 			(blue * mLightModifier).toInt()
 		)
+	}
+
+	private fun Paint.computeAlpha(enabled: Boolean) {
+		alpha = if (enabled) {
+			(alpha * mProgress).toInt()
+		} else {
+			(alpha * mProgress * mOpacity).toInt()
+		}
+	}
+
+	private fun Paint.computeBackStrokeAlpha(enabled: Boolean) {
+		alpha = if (enabled) {
+			alpha
+		} else {
+			(alpha * mOpacity).toInt()
+		}
 	}
 }
